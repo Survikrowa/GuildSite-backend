b3ffc1e9f446f07ca7ddea91fc36d4e72111fc17
Survikrowa <survikrowa@o2.pl>
2020-07-25 00:35:38 +0200

    Basic index.ts files with bodyParser and 1st route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const express = require("express");
+import bodyParser from "body-parser";
+import express from "express";
+import { userRouter } from "./router/userRouter";
+import { checkDB } from "./models/user";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", userRouter);
+
+app.listen(port, checkDB);

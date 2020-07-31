901475aea6dd905743414f7401eafe83dafdd058
Survikrowa <survikrowa@o2.pl>
2020-07-31 21:57:24 +0200

    I dont know why i had this but well its better now

--- src/services/createNewUser.ts
+++ src/services/createNewUser.ts
@@ -3,21 +3,19 @@ import bcrypt from "bcrypt";
 import { createUser } from "../models/user";
 const SALT_ROUNDS = 10;
 
-export const createNewUser = async (req: Request) => {
-  await bcrypt.hash(req.body.password, SALT_ROUNDS, async (err, hash) => {
-    if (!err) {
-      await createUser({
-        username: req.body.username,
-        password: hash,
-        eMail: req.body.email,
-      });
-      return {
-        message: "Account has been created!",
-        status: 203,
-        redirect: true,
-      };
-    } else {
-      return err;
-    }
-  });
+export const createNewUser = async (req: Request, authCodeId: number) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+  await createUser(
+    {
+      username: req.body.username,
+      password: hashedPassword,
+      eMail: req.body.email,
+    },
+    authCodeId
+  );
+  return {
+    message: "Account has been created!",
+    status: 203,
+    redirect: true,
+  };
 };

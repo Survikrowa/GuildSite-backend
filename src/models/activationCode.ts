7d090896460e4eb1322365641b5684066ee04ed0
Survikrowa <survikrowa@o2.pl>
2020-07-31 21:57:57 +0200

    Add ActivationCodes model

---
+++ src/models/activationCode.ts
@@ -0,0 +1,50 @@
+import { Sequelize, Model, DataTypes, NOW } from "sequelize";
+
+const sequelize = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP}/${process.env.DB_NAME}`
+);
+
+class activationCodes extends Model {
+  public authCodeId!: number;
+  public activationCode!: string;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+activationCodes.init(
+  {
+    authCodeId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    activationCode: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: NOW,
+    },
+  },
+  {
+    tableName: "activationCodes",
+    sequelize,
+  }
+);
+
+export const insertActivationCode = async (activationCode: string) => {
+  try {
+    return activationCodes.create({
+      activationCode: activationCode,
+    });
+  } catch (error) {
+    return error;
+  }
+};

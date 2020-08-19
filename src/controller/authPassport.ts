5bb597c5794026ccaa3e1aa436a89ed796585839
Survikrowa <survikrowa@o2.pl>
2020-08-19 15:20:12 +0200

    Fix passport not setting cookie for user

--- src/controller/authPassport.ts
+++ src/controller/authPassport.ts
@@ -4,6 +4,13 @@ import passport from "passport";
 export const authPassportController: RequestHandler = (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) return next(err);
-    if (!user) return res.status(401).json({ message: info.message });
+    if (!user) {
+      return res.status(401).json({ message: info.message });
+    } else {
+      req.logIn(user, (err) => {
+        if (err) return next(err);
+      });
+      return res.status(200).json({ Status: 201, Redirect: true });
+    }
   })(req, res, next);
 };

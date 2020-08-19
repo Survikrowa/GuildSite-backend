c767b1addcbf10628c9afd0452c94785a530556c
Survikrowa <survikrowa@o2.pl>
2020-08-19 15:21:01 +0200

    Add error if user tries to login as a unauthenticated

--- src/services/passportLocalStrategy.ts
+++ src/services/passportLocalStrategy.ts
@@ -2,7 +2,7 @@ import { Strategy as LocalStrategy } from "passport-local";
 import { findUserByUsername } from "../models/user";
 import bcrypt from "bcrypt";
 import { validateLoginData } from "./userValidation";
-import { ZodError } from "zod";
+import type { ZodError } from "zod";
 
 export const strategy = new LocalStrategy(async (username, password, done) => {
   try {
@@ -11,6 +11,8 @@ export const strategy = new LocalStrategy(async (username, password, done) => {
       const user = await findUserByUsername(username);
       if (!user) {
         return done(null, false, { message: "Invalid username or password" });
+      } else if (!user.authenticated) {
+        return done(null, false, { message: "Account is not authenticated" });
       } else {
         const hashedPassword = user.get("password");
         const isPasswordMatch = await bcrypt.compare(password, hashedPassword);

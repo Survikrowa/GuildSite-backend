a0274645d505d5d804ec84e95236dba52bc4d422
Survikrowa <survikrowa@o2.pl>
2020-08-24 22:18:21 +0200

    Remove unnecessary try/catch

--- src/services/databaseServices/findUserBy.ts
+++ src/services/databaseServices/findUserBy.ts
@@ -1,13 +1,7 @@
-import { logger } from "../errorLogger";
 import { User } from "../../models/user";
 
 export const findUserBy = async (conditions: Record<string, string>) => {
-  try {
-    return User.findOne({
-      where: conditions,
-    });
-  } catch (error) {
-    logger.error(error);
-    return;
-  }
+  return User.findOne({
+    where: conditions,
+  });
 };

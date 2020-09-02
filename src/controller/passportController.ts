059b0151a96c81f5ad844453e490e7c40babc329
Survikrowa <survikrowa@o2.pl>
2020-09-02 09:27:39 +0200

    Remove unnecessary else

--- src/controller/passportController.ts
+++ src/controller/passportController.ts
@@ -4,14 +4,11 @@ import passport from "passport";
 export const localPassportController: RequestHandler = (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) return next(err);
-    if (!user) {
-      return res.status(401).json({ message: info.message });
-    } else {
-      req.logIn(user, (err) => {
-        if (err) return next(err);
-        return res.status(200).json({ Status: 200, Redirect: true });
-      });
-    }
+    if (!user) return res.status(401).json({ message: info.message });
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.status(200).json({ Status: 200, Redirect: true });
+    });
   })(req, res, next);
 };
 export const facebookPassportController: RequestHandler = (req, res, next) => {

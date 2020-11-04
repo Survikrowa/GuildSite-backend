d5891d3a62660c57c7159fe29ec4d85051a43884
Survikrowa <szymonhrabia60@gmail.com>
2020-11-04 21:54:17 +0100

    Fix perma status 500 bug with passport-local

--- src/controller/passportController.ts
+++ src/controller/passportController.ts
@@ -2,14 +2,16 @@ import type { RequestHandler } from "express";
 import passport from "passport";
 
 export const localPassportController: RequestHandler = (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(401).json({ message: info.message });
-    req.logIn(user, (err) => {
-      if (err) return next(err);
-      return res.status(200).json({ Status: 200, Redirect: true });
-    });
-  })(req, res, next);
+  return new Promise((_resolve, reject) => {
+    passport.authenticate("local", (err, user, info) => {
+      if (err) return reject(err);
+      if (!user) return res.status(401).json({ message: info.message });
+      req.logIn(user, (err) => {
+        if (err) return reject(err);
+        return res.status(200).json({ Status: 200, Redirect: true });
+      });
+    })(req, res, next);
+  });
 };
 export const facebookPassportController: RequestHandler = (req, res, next) => {
   passport.authenticate("facebook", {

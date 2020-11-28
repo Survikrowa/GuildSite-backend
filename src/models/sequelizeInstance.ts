8bd0bd53d2c0f0bf41b0f4a595676f809b7d5e1e
Rdzio <jasjedrzejowski@gmail.com>
2020-11-28 20:04:31 +0100

    .

--- src/models/sequelizeInstance.ts
+++ src/models/sequelizeInstance.ts
@@ -1,5 +1,16 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  <string>process.env.DATABASE_URL
-);
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_IP,
+  port: Number(process.env.DB_PORT),
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});

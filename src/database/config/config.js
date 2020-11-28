4e101edee957e3abc24b2f567100ecb0a8a34f31
Rdzio <jasjedrzejowski@gmail.com>
2020-11-28 20:36:13 +0100

    .

---
+++ src/database/config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  dev: {
+    username: 'tpfhfuorubtzdf',
+    password: '61cb214e1b42800d755ecc10ffc2099f9d10f47616defe41e9dbf44c700773c5',
+    database: 'd8j9ml9u3ceq4l',
+    host: 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: 'true',
+        rejectUnauthorized: 'false',
+      },
+    },
+  },
+  production: {
+    username: 'tpfhfuorubtzdf',
+    password: '61cb214e1b42800d755ecc10ffc2099f9d10f47616defe41e9dbf44c700773c5',
+    database: 'd8j9ml9u3ceq4l',
+    host: 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: 'true',
+        rejectUnauthorized: 'false',
+      },
+    },
+  },
+};

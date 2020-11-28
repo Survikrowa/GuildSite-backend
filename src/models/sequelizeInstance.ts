3c563f3d36bb00a077e6ac69f4aa963654add9bb
Rdzio <jasjedrzejowski@gmail.com>
2020-11-28 19:22:43 +0100

    .

--- src/models/sequelizeInstance.ts
+++ src/models/sequelizeInstance.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP}/${process.env.DB_NAME}`
+  <string>process.env.DATABASE_URL
 );
